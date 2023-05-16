cc50522817dab51d0d791f71e2aa2b179ff458a9
Shafi <shadikulshafi@yahoo.com>
2023-05-17 00:19:36 +0600

    header part complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lgoin_ui/view/screen/log_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  Scaffold(
-        backgroundColor: Colors.white,
-      )
+      home: LogInScreen(),
     );
   }
 }
---
+++ lib/view/screen/log_in.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatelessWidget {
+  const LogInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: Image(
+                        image: AssetImage('assets/images/logo.png'),
+                        fit: BoxFit.cover,
+                      )),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Maintenance',
+                        style:
+                            TextStyle(fontSize: 24, fontFamily: 'Rubik Medium'),
+                      ),
+                      Text(
+                        'Box',
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontFamily: 'Rubik Medium',
+                            color: Color(0xffe1480d)),
+                        textAlign: TextAlign.left,
+                      )
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Text(
+                'Log in',
+                style: TextStyle(fontSize: 25, fontFamily: 'Rubik Medium'),
+              ),
+              const Text(
+                'Lorem ipsum quia dolor sit amet, \n consectetur adipisci velit.',
+                style: TextStyle(fontSize: 15, fontFamily: 'Rubik Regular'),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
